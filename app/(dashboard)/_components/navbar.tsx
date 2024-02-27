43dc8fe302014236de93a762abbb0bc5134f1903
Halil <halillkesgin@gmail.com>
2024-02-27 23:41:21 +0300

    search input done

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,13 +2,15 @@
 
 import { UserButton } from "@clerk/nextjs"
 
+import { SearchInput } from "./search-input"
+
 export const Navbar = () => {
     return (
         <div className="flex items-center gap-x-4 p-5">
             <div className="hidden lg:flex lg:flex-1">
-                Search
+                <SearchInput />
             </div>
-            <UserButton  afterSignOutUrl="/" />
+            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
