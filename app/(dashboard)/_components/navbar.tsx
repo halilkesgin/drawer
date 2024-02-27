8b3faeed2b9dbf180d2d6adf580d67f7b400fde8
Halil <halillkesgin@gmail.com>
2024-02-27 23:45:54 +0300

    invite button done

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 import { SearchInput } from "./search-input"
+import { InviteButton } from "./invite-button"
 
 export const Navbar = () => {
     return (
@@ -10,6 +11,31 @@ export const Navbar = () => {
             <div className="hidden lg:flex lg:flex-1">
                 <SearchInput />
             </div>
+            <div className="block lg:hidden flex-1">
+                <OrganizationSwitcher 
+                    hidePersonal
+                    appearance={{
+                        elements: {
+                            rootBox: {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "100%",
+                                maxWidth: "376px"
+                            },
+                            organizationSwitcherTrigger: {
+                                padding: "6px",
+                                width: "100%",
+                                borderRadius: "8px",
+                                border: "1px solid #e5e7eb",
+                                justifyContent: "space-between",
+                                backgroundColor: "white"
+                            }
+                        }
+                    }}
+                />
+            </div>
+            <InviteButton />
             <UserButton afterSignOutUrl="/" />
         </div>
     )
