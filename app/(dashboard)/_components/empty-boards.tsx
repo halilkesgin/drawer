68f9f7e4beea0722be5b7b7f73726ef26573b3a6
Halil <halillkesgin@gmail.com>
2024-02-28 20:00:21 +0300

    search queries done

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -3,12 +3,14 @@
 import { AlertOctagon } from "lucide-react"
 import { useOrganization } from "@clerk/nextjs"
 import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
 import { api } from "@/convex/_generated/api"
 import { Button } from "@/components/ui/button"
 import { useApiMutation } from "@/hooks/use-api-mutation"
 
 export const EmptyBoards = () => {
+    const router = useRouter()
     const { organization } = useOrganization()
     const { mutate, pending } = useApiMutation(api.board.create)
 
@@ -21,6 +23,7 @@ export const EmptyBoards = () => {
         })
             .then((id) => {
                 toast.success("Board created")
+                router.push(`/board/${id}`)
             })
             .catch(() => {
                 toast.error("Failed to create board")
--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -6,6 +6,7 @@ import { Plus } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { api } from "@/convex/_generated/api"
 import { useApiMutation } from "@/hooks/use-api-mutation"
+import { useRouter } from "next/navigation"
 
 interface NewBoardButtonProps {
     orgId: string
@@ -16,6 +17,7 @@ export const NewBoardButton = ({
     orgId,
     disabled
 }: NewBoardButtonProps) => {
+    const router = useRouter()
     const { mutate, pending } = useApiMutation(api.board.create)
 
     const onClick = () => {
@@ -25,6 +27,7 @@ export const NewBoardButton = ({
         })
             .then((id) => {
                 toast.success("Board created")
+                router.push(`/board/${id}`)
             })
             .catch(() => {
                 toast.error("Failed to create board")
@@ -34,7 +37,7 @@ export const NewBoardButton = ({
     return (
         <button
             disabled={pending || disabled}
-            onClick={() => {}}
+            onClick={onClick}
             className={cn(
                 "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
                 (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
