2afdeb499f9dffcaa7dbb2d53fb1825e9a0f26c2
Halil <halillkesgin@gmail.com>
2024-02-28 20:39:06 +0300

    canvas layout done

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,21 @@
 "use client"
 
-export const Canvas = () => {
+import { Info } from "./info"
+import { Participants } from "./participants"
+import { Toolbar } from "./toolbar"
+
+interface CanvasProps {
+    boardId: string
+}
+
+export const Canvas = ({
+    boardId
+}: CanvasProps) => {
     return (
-        <main
-            className="h-full w-full relative bg-neutral-100 tounch-none"
-        >
-            Canvas!
+        <main  className="h-full w-full relative bg-neutral-100 touch-none">
+            <Info />
+            <Participants />
+            <Toolbar />
         </main>
     )
 }
---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,7 @@
+export const Participants = () => {
+    return (
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-lg p-3 flex items-center shadow-md">
+            List of users
+        </div>
+    )
+}
